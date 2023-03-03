e3c36affd793a4f5066bce76d833c8058fefa3e2
MBundela <113464403+MBundela@users.noreply.github.com>
2023-03-03 23:42:01 +0530

    First

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+  const nav3 = document.querySelectorAll('.nav3 ul li a i')
+    for(const nav3item of nav3){
+        nav3item.addEventListener('mouseover',function(){
+            nav3item.setAttribute('style','transform: rotate(360deg);')
+        })
+
+        nav3item.addEventListener
+    }
